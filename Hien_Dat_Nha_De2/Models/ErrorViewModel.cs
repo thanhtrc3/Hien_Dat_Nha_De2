3d6021f7fc366df745254c2428d73001b96db028
VITUS <VITUS@DESKTOP-RMJBG58>
2026-03-20 13:24:28 +0700

    Add project files.

---
+++ Hien_Dat_Nha_De2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Hien_Dat_Nha_De2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
